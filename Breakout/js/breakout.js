2305d1a2ff2bf8d74b778f23439c9d78dc6ddfe6
Paolo Zesati <a01784391@tec.mx>
2025-05-01 00:50:29 -0400

    breakout.js

--- Breakout/js/breakout.js
+++ Breakout/js/breakout.js
@@ -158,7 +158,7 @@ class Game {
             }
         
 
-        ctx.fillStyle = "red";
+        ctx.fillStyle = "red";h
         ctx.font = "20px Arial";
         ctx.fillText(`VIDAS: ${this.lives}`, 10, 20);
         ctx.fillText(`BLOQUES DESTRUIDOS: ${this.destroyedBlocks}`, 10, 40);
